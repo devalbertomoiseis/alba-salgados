5f9777920fae1084c4992d65a7ce386cb8cfb52c
albertomoiseis <contatoalbertomoiseis@gmail.com>
2023-08-29 20:22:30 -0300

    first commit

---
+++ js/javascript.js
@@ -0,0 +1,27 @@
+const btnMobile = document.getElementById("btn-mobile");
+
+function toggleMenu(event) {
+  if (event.type == "touchstart") event.preventDefault();
+  const nav = document.getElementById("nav");
+  nav.classList.toggle("active");
+}
+
+btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("touchstart", toggleMenu);
+
+/* */
+
+const accordionHeaders = document.querySelectorAll(".accordion-header");
+
+accordionHeaders.forEach((header) => {
+  header.addEventListener("click", () => {
+    const accordionItem = header.parentElement;
+    const accordionContent = accordionItem.querySelector(".accordion-content");
+
+    if (accordionContent.style.display === "block") {
+      accordionContent.style.display = "none";
+    } else {
+      accordionContent.style.display = "block";
+    }
+  });
+});
